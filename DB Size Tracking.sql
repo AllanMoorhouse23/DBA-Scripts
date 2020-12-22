71bfe00b5722ec9cf7ce099cc3811e8c30e08f79
Allan Moorhouse <allanmoorhouse.am@gmail.com>
2020-12-23 09:55:07 +1300

    Add files via upload
    Added script for tracking DB sizes over time.

---
+++ DB Size Tracking.sql
@@ -0,0 +1,112 @@
+/*Run the following section first*/
+
+USE [/**DB TO BE INSTALLED ON**/]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DB_Size_Tracking](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[DB_ID] [int] NOT NULL,
+	[DB_Name] [varchar](50) NOT NULL,
+	[Size] [int] NOT NULL,
+	[Date] [datetime] NOT NULL,
+ CONSTRAINT [PK_DB_Size_Tracking] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[DB_Size_Tracking] ADD  CONSTRAINT [DF_DB_Size_Tracking_Date]  DEFAULT (getdate()) FOR [Date]
+GO
+
+
+
+
+/*Run this section second*/
+
+CREATE PROCEDURE sp_update_DB_Size_Tracking
+AS
+INSERT INTO DB_Size_Tracking (DB_ID, DB_NAME, Size)
+SELECT databases.database_id AS [DB_ID], databases.name AS [DB_Name], CONVERT(VARCHAR, sum(size)*8/1024) AS [Current Size - MB] 
+FROM sys.databases 
+JOIN sys.master_files ON databases.database_id=master_files.database_id 
+GROUP BY sys.databases.database_id, sys.databases.name;
+
+
+
+
+/*Run this section last*/
+
+USE [msdb]
+GO
+
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Update_DB_Size_Tracking', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'Runs a stored procedure to update the DB sizes table to track db size', 
+		@category_name=N'Data Collector', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Execute stored procedure', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'exec sp_update_DB_Size_Tracking;', 
+		@database_name=N'dba', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Daily at 5pm', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20201221, 
+		@active_end_date=99991231, 
+		@active_start_time=170000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'732b4193-f071-4676-a62b-72f9f0599c60'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
